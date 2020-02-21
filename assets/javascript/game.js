c8405e7a363ca51d8dc358d066a77ef6b2e2948a
Jennifer Kruk <jenkruk@gmail.com>
2020-02-21 12:01:58 -0700

    cleaning up code and correcting bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,32 +10,81 @@ var flowers = [
 ];
 
 var trees = [
-  'apple’, ‘laurel’, ‘bonsai’, ‘brazil’, ‘cacao’, ‘cherry', 'christmas', 'coconut’, ‘cottonwood’, ‘cypress’, ‘dogwood’, ‘family’, ‘ficus’, ‘fiddleleaf’, ‘sequoia’, ‘juniper’, ‘mahogany’, ‘palm’, ‘pine’, ‘pistachio’, ‘redwood’, ‘rubber’, ‘olive’, ’spruce’, ‘maple’, ‘sycamore’, ‘walnut’, ‘cedar’, ‘willow'
+  'apple', 'laurel', 'bonsai', 'brazil', 'cacao', 'cherry', 'christmas', 'coconut'
 ];
 
 var critters = [
-  'butterfly', 'ladybug’, ‘caterpillar’, ‘bee’, ‘bluebird’, ‘sparrow’, ‘woodpecker’, ‘hummingbird’, ‘mockingbird, ‘beetle’, ‘cardinal’, ‘meadowlark’, ‘cricket’, ‘dragonfly’, ‘falcon’, ‘swallow’, ‘grasshopper’, ‘mosquito’, ‘mantis’, ‘owl’, ‘stinkbug’, ‘weevil’, ‘centipede’, ‘inchworm’, ‘parrot’, ‘lovebirds’, ‘flamingo’, ‘penguin’, ‘chicken’, ‘duckling’, ‘spider'
+  'butterfly', 'ladybug', 'caterpillar', 'bee', 'bluebird', 'sparrow', 'woodpecker'
 ];
 
 var songs = [
   'happy', 'anticipation', 'bathwater', 'dreams', 'luka', 'promises', 'valentine', 'why'
 ];
 
-var sound = document.getElementById('sound');
+function generateButtons() {
+  let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+    `
+      <button
+        class="btn keyboard btn-success text-uppercase"
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
+  document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
 
-document.getElementById("wordSpotlight").style.textTransform = "uppercase";
+function guessedWord() {
+  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : ' _ ')).join("");
+
+  document.getElementById("wordSpotlight").innerHTML = wordStatus;
+}
+
+function updateMistakes() {
+  document.getElementById('mistakes').innerHTML = mistakes;
+  document.getElementById('mistakes').style.color = "#dc3545";
+}
+
+function updateTreePicture() {
+  document.getElementById('treePic').src = "./assets/images/" + mistakes + '.jpg';
+}
+
+function checkIfGameWon() {
+  if (wordStatus === answer) {
+    document.getElementById('keyboard').innerHTML = 'You Won, Smarty Pants!';
+    document.getElementById('treePic').src = "./assets/images/0.jpg";
+    var sound = document.getElementById('sound');
+    sound.play();
+  }
+}
+
+function checkIfGameLost() {
+  if (mistakes === maxWrong) {
+    document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
+    document.getElementById('keyboard').innerHTML = 'You\'ll get it next time!';
+  }
+}
 
 /* functions that allow each button link to it's intended category */
 function main() {
   document.getElementById("main").style.display = "block";
   document.getElementById("play").style.display = "none";
-  sound.pause();
+}
+
+function randomWord() {
+  answer = critters[Math.floor(Math.random() * critters.length)];
 }
 
 function play() {
   document.getElementById("main").style.display = "none";
   document.getElementById("play").style.display = "block";
   document.getElementByClassName("gameTitle").style.display = "none";
+  document.getElementById("wordSpotlight").style.textTransform = "uppercase";
+  document.getElementById('maxWrong').innerHTML = maxWrong;
+  randomWord();
+  generateButtons();
+  guessedWord();
 }
 
 function flower() {
@@ -74,40 +123,6 @@ function song() {
   updateTreePicture();
 }
 
-function randomWord() {
-  answer = flowers[Math.floor(Math.random() * flowers.length)];
-}
-
-function generateButtons() {
-  let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
-    `
-      <button
-        class="btn keyboard btn-success text-uppercase"
-        id='` + letter + `'
-        onClick="handleGuess('` + letter + `')"
-      >
-        ` + letter + `
-      </button>
-    `).join('');
-
-  document.getElementById('keyboard').innerHTML = buttonsHTML;
-}
-
-function guessedWord() {
-  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : ' _ ')).join("");
-
-  document.getElementById("wordSpotlight").innerHTML = wordStatus;
-}
-
-function updateMistakes() {
-  document.getElementById('mistakes').innerHTML = mistakes;
-  document.getElementById('mistakes').style.color = "#dc3545";
-}
-
-/* shows how many misses are left */
-document.getElementById('maxWrong').innerHTML = maxWrong;
-
-
 function handleGuess(chosenLetter) {
   guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
   document.getElementById(chosenLetter).setAttribute('disabled', true);
@@ -123,25 +138,19 @@ function handleGuess(chosenLetter) {
   }
 }
 
-/* Changes picture every miss */
-function updateTreePicture() {
-  document.getElementById('treePic').src = "./assets/images/" + mistakes + '.jpg';
-}
-
-
-function checkIfGameWon() {
-  if (wordStatus === answer) {
-    document.getElementById('keyboard').innerHTML = 'You Won, Smarty Pants!';
-    document.getElementById('treePic').src = "./assets/images/0.jpg";
-    sound.play();
-  }
-}
-
-function checkIfGameLost() {
-  if (mistakes === maxWrong) {
-    document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
-    document.getElementById('keyboard').innerHTML = 'You\'ll get it next time!';
-  }
+function tryAgain() {
+  document.getElementById("main").style.display = "none";
+  document.getElementById("play").style.display = "block";
+  document.getElementById('treePic').src = "./assets/images/0.jpg";
+  answer = "";
+  maxWrong = 7;
+  mistakes = 0;
+  guessed = [];
+  wordStatus = null;
+  updateMistakes();
+  randomWord();
+  generateButtons();
+  guessedWord();
 }
 
 function backToMain() {
@@ -157,25 +166,4 @@ function backToMain() {
   randomWord();
   generateButtons();
   guessedWord();
-  sound.pause();
 }
-
-function tryAgain() {
-  document.getElementById("main").style.display = "none";
-  document.getElementById("play").style.display = "block";
-  document.getElementById('treePic').src = "./assets/images/0.jpg";
-  answer = "";
-  maxWrong = 7;
-  mistakes = 0;
-  guessed = [];
-  wordStatus = null;
-  updateMistakes();
-  randomWord();
-  generateButtons();
-  guessedWord();
-  sound.pause();
-}
-
-randomWord();
-generateButtons();
-guessedWord();
